45b5fc142300330fde810239bce0c1fa7b5cd9ab
Jacques <pedro.jacques.academico@gmail.com>
2023-03-31 22:57:36 -0300

    inicio do projeto

--- nossoJogo.js
+++ nossoJogo.js
@@ -10,3 +10,37 @@
  * 
  * 
  */
+
+    console.log("Bem-vindo(a) ao jogo de blackjack");
+
+    let jogo = confirm("Quer iniciar uma nova rodada?");
+    
+    if (jogo) {
+      const cartaUserUm = comprarCarta();
+      const cartaUserDois = comprarCarta();
+      const cartaPcUm = comprarCarta();
+      const cartaPcDois = comprarCarta();
+    
+      const resultadoCartasUser = cartaUserUm.valor + cartaUserDois.valor;
+      const resultadoCartasPc = cartaPcUm.valor + cartaPcDois.valor;
+    
+      console.log(`Usuário - cartas: ${cartaUserUm.texto} ${cartaUserDois.texto} - pontuação é ${resultadoCartasUser}`);
+      console.log(`Computador - cartas: ${cartaPcUm.texto} ${cartaPcDois.texto} - pontuação é ${resultadoCartasPc}`);
+      
+      if (resultadoCartasUser > resultadoCartasPc) {
+        console.log("O usuário ganhou!");
+      } else if (resultadoCartasPc > resultadoCartasUser) {
+        console.log("O computador ganhou!");
+      } else {
+        console.log("Empate!");
+      }
+    } else {
+      console.log("O jogo acabou");
+    }
+    
+    
+
+
+
+
+
